7889d25da53fe2fba7ee84ebffa4249dc099961b
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-11-02 19:32:07 +0600

    lesson_03

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -3,30 +3,89 @@ package com.sumin.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.sumin.vknewsclient.ui.PostCard
-import com.sumin.vknewsclient.ui.theme.VkNewsClientTheme
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            VkNewsClientTheme {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                        .padding(8.dp)
-                ) {
-                    PostCard()
-                }
-            }
+//            VkNewsClientTheme {
+//                Box(
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .background(MaterialTheme.colors.background)
+//                        .padding(8.dp)
+//                ) {
+//                    PostCard()
+//                }
+//            }
+
+            Test()
         }
     }
+}
+
+@Preview
+@Composable
+private fun Test() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+//        Example1()
+//        Example2()
+        Example3()
+    }
+}
+
+@Composable
+private fun Example1() {
+    OutlinedButton(onClick = { /*TODO*/ }) {
+        Text(text = "Hello World")
+    }
+}
+
+@Composable
+private fun Example2() {
+    TextField(
+        value = "",
+        onValueChange = { },
+        label = {
+            Text(text = "Label")
+        },
+        singleLine = true
+    )
+}
+
+@Composable
+private fun Example3() {
+    AlertDialog(
+        onDismissRequest = { },
+        title = {
+            Text(text = "Are you sure?")
+        },
+        text = {
+            Text(text = "Do you want to delete this file?")
+        },
+        confirmButton = {
+            TextButton(
+                onClick = { }
+            ) {
+                Text(text = "Yes")
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { }
+            ) {
+                Text(text = "No")
+            }
+        }
+    )
 }
