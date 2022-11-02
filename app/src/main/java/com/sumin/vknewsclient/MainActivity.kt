d3b80a0a51fb991bbf11e2c72d0a82176bb2b36d
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-11-02 20:04:13 +0600

    lesson_04

--- app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
+++ app/src/main/java/com/sumin/vknewsclient/MainActivity.kt
@@ -3,13 +3,18 @@ package com.sumin.vknewsclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,54 +43,65 @@ private fun Test() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-//        Example1()
-//        Example2()
-        Example3()
+        Example1()
     }
 }
 
 @Composable
 private fun Example1() {
-    OutlinedButton(onClick = { /*TODO*/ }) {
-        Text(text = "Hello World")
-    }
-}
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "TopAppBar title")
+                },
+                navigationIcon = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            val items = listOf("1", "2", "3")
+            BottomNavigation {
+                items.forEachIndexed { index, item ->
+                    val icon = when (index) {
+                        0 -> Icons.Filled.Favorite
+                        1 -> Icons.Filled.Edit
+                        else -> Icons.Filled.Delete
+                    }
 
-@Composable
-private fun Example2() {
-    TextField(
-        value = "",
-        onValueChange = { },
-        label = {
-            Text(text = "Label")
-        },
-        singleLine = true
-    )
-}
-
-@Composable
-private fun Example3() {
-    AlertDialog(
-        onDismissRequest = { },
-        title = {
-            Text(text = "Are you sure?")
-        },
-        text = {
-            Text(text = "Do you want to delete this file?")
-        },
-        confirmButton = {
-            TextButton(
-                onClick = { }
-            ) {
-                Text(text = "Yes")
+                    BottomNavigationItem(
+                        icon = {
+                            Icon(
+                                imageVector = icon,
+                                contentDescription = null
+                            )
+                        },
+                        label = {
+                            Text(text = item)
+                        },
+                        selected = index == 0,
+                        onClick = { }
+                    )
+                }
             }
         },
-        dismissButton = {
-            TextButton(
-                onClick = { }
-            ) {
-                Text(text = "No")
-            }
+        drawerContent = {
+            Text(text = "Text 1")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Text 2")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "Text 3")
         }
-    )
+    ) {
+        Text(
+            modifier = Modifier.padding(it),
+            text = "This is scaffold content"
+        )
+    }
 }
