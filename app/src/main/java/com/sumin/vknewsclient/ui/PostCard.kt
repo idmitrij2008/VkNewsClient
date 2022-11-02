ca4f840110d838fce1272c5b304b08b9358dec13
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-11-02 17:20:23 +0600

    lesson_02

--- app/src/main/java/com/sumin/vknewsclient/ui/PostCard.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/PostCard.kt
@@ -13,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sumin.vknewsclient.R
@@ -23,37 +25,106 @@ import com.sumin.vknewsclient.ui.theme.VkNewsClientTheme
 @Composable
 internal fun PostCard() {
     Card {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            PostHeader()
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = stringResource(R.string.template_text)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
             Image(
-                modifier = Modifier
-                    .size(50.dp)
-                    .clip(CircleShape),
-                painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+                modifier = Modifier.fillMaxWidth(),
+                painter = painterResource(id = R.drawable.post_content_image),
+                contentScale = ContentScale.FillWidth,
                 contentDescription = null
             )
-            Spacer(modifier = Modifier.width(8.dp))
-            Column(modifier = Modifier.weight(1f)) {
-                Text(
-                    text = "/dev/null",
-                    color = MaterialTheme.colors.onPrimary
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = "14:00",
-                    color = MaterialTheme.colors.onSecondary
-                )
-            }
-            Icon(
-                imageVector = Icons.Rounded.MoreVert,
-                tint = MaterialTheme.colors.onSecondary,
-                contentDescription = null,
+            Spacer(modifier = Modifier.height(8.dp))
+            Statistics()
+        }
+
+    }
+}
+
+@Composable
+private fun Statistics() {
+    Row {
+        Row(modifier = Modifier.weight(1f)) {
+            IconWithText(
+                iconResId = R.drawable.ic_views_count,
+                text = "14"
             )
         }
+        Row(
+            modifier = Modifier.weight(1f),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            IconWithText(
+                iconResId = R.drawable.ic_share,
+                text = "8"
+            )
+            IconWithText(
+                iconResId = R.drawable.ic_comment,
+                text = "2"
+            )
+            IconWithText(
+                iconResId = R.drawable.ic_like,
+                text = "23"
+            )
+        }
+    }
+}
+
+@Composable
+private fun IconWithText(
+    iconResId: Int,
+    text: String
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = iconResId),
+            tint = MaterialTheme.colors.onSecondary,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = text,
+            color = MaterialTheme.colors.onSecondary
+        )
+    }
+}
+
+@Composable
+private fun PostHeader() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Image(
+            modifier = Modifier
+                .size(50.dp)
+                .clip(CircleShape),
+            painter = painterResource(id = R.drawable.post_comunity_thumbnail),
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column(modifier = Modifier.weight(1f)) {
+            Text(
+                text = "/dev/null",
+                color = MaterialTheme.colors.onPrimary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = "14:00",
+                color = MaterialTheme.colors.onSecondary
+            )
+        }
+        Icon(
+            imageVector = Icons.Rounded.MoreVert,
+            tint = MaterialTheme.colors.onSecondary,
+            contentDescription = null,
+        )
     }
 }
 
