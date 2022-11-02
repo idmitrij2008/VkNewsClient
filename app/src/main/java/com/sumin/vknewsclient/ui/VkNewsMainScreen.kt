733f05285cb5aea5547167d71591b4856b8dcbaf
Журба Дмитрий <dmitriy.zhurba@gazprombank.ru>
2022-11-02 23:00:55 +0600

    lesson_06

--- app/src/main/java/com/sumin/vknewsclient/ui/VkNewsMainScreen.kt
+++ app/src/main/java/com/sumin/vknewsclient/ui/VkNewsMainScreen.kt
@@ -1,14 +1,49 @@
 package com.sumin.vknewsclient.ui
 
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.res.stringResource
+import kotlinx.coroutines.launch
 
 @Composable
 internal fun MainScreen() {
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = SnackbarHostState()
+    val fabIsVisible = remember { mutableStateOf(true) }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
+        floatingActionButton = {
+            if (fabIsVisible.value) {
+                FloatingActionButton(
+                    onClick = {
+                        scope.launch {
+                            val action = snackBarHostState.showSnackbar(
+                                message = "This is Snackbar",
+                                actionLabel = "Hide FAB",
+                                duration = SnackbarDuration.Long
+                            )
+
+                            if (action == SnackbarResult.ActionPerformed) {
+                                fabIsVisible.value = false
+                            }
+                        }
+                    }
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Favorite,
+                        contentDescription = null
+                    )
+                }
+            }
+        },
         bottomBar = {
             BottomNavigation {
                 val selectedItemPosition = remember { mutableStateOf(0) }
@@ -37,7 +72,7 @@ internal fun MainScreen() {
                     )
                 }
             }
-        }
+        },
     ) {
 
     }
